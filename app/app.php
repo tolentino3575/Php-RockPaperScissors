14278b573bfef323524596ad83692a50115ab6a1
erik tolentino and michael lor <tolentino3575@gmail.com>
2016-02-17 09:44:11 -0800

    saving our project

--- app/app.php
+++ app/app.php
@@ -1,14 +1,24 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Person.php";
+    require_once __DIR__."/../src/rockpaperscissors.php";
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'
+        'twig.path' => __DIR__.'/../views'));
 
+    $app->get("/", function() use ($app) {
+         return $app['twig']->render('form.html.twig', array('winner'=>NULL));
+     });
+
+     $app->get("/pvp", function() use ($app){
+         $my_Game = new RockPaperScissors;
+         $result = $my_Game->playGame($_GET['player1'], $_GET['player2']);
+         var_dump($result);
+         return $app['twig']->render('rockpaperscissors.html.twig', array('winner'=>$result));
+     });
 
     return $app;
-));
+
 
 ?>
--- src/rockpaperscissors.php
+++ src/rockpaperscissors.php
@@ -3,25 +3,16 @@
 
     class RockPaperScissors
     {
+        // $values = rand(0, 2);
+        // $computer = array(0=>"rock",1=>"paper",2=>"scissors");
+        // $new_value = $computer([$values]);
 
         function playGame($first_input, $second_input)
         {
-            if (($first_input == "rock")&&($second_input == "scissors"))
+            if ((($first_input == "rock")&&($second_input == "scissors")) || (($first_input == "scissors")&&($second_input =="paper")) || (($first_input == "paper")&&($second_input =="rock")))
             {
                 return "Player 1";
-            } elseif (($first_input == "scissors")&&($second_input =="rock"))
-            {
-                return "Player 2";
-            } elseif (($first_input == "paper")&&($second_input =="scissors"))
-            {
-                return "Player 2";
-            } elseif (($first_input == "scissors")&&($second_input =="paper"))
-            {
-                return "Player 1";
-            } elseif (($first_input == "paper")&&($second_input =="rock"))
-            {
-                return "Player 1";
-            } elseif (($first_input == "rock")&&($second_input =="paper"))
+            } elseif ((($first_input == "scissors")&&($second_input =="rock")) || (($first_input == "paper")&&($second_input =="scissors")) || (($first_input == "rock")&&($second_input =="paper")))
             {
                 return "Player 2";
             } elseif ($first_input == $second_input)
@@ -32,6 +23,29 @@
     }
 
 
+
+    // if (($first_input == "rock")&&($second_input == "scissors"))
+    // {
+    //     return "Player 1";
+    // } elseif (($first_input == "scissors")&&($second_input =="rock"))
+    // {
+    //     return "Player 2";
+    // } elseif (($first_input == "paper")&&($second_input =="scissors"))
+    // {
+    //     return "Player 2";
+    // } elseif (($first_input == "scissors")&&($second_input =="paper"))
+    // {
+    //     return "Player 1";
+    // } elseif (($first_input == "paper")&&($second_input =="rock"))
+    // {
+    //     return "Player 1";
+    // } elseif (($first_input == "rock")&&($second_input =="paper"))
+    // {
+    //     return "Player 2";
+    // } elseif ($first_input == $second_input)
+    // {
+    //     return "Draw";
+    // }
 
 
 
